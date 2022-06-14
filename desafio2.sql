5ff80d715b5285031152bbb01a29ea34705f1b80
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-06-14 18:06:49 -0300

    Cumpre requisito 2

---
+++ desafio2.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(DISTINCT musica.id_musica) AS cancoes, COUNT(DISTINCT album.id_artista) AS artistas, COUNT(DISTINCT musica.id_album) AS albuns
+FROM SpotifyClone.musica AS musica
+INNER JOIN SpotifyClone.album AS album;
