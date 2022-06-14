bc14df6b776fb6cb839ac88e8c8d28e094c36074
Lucas Pereira Castanheira Nascimento <lucaspereira.cn@hotmail.com>
2022-06-14 18:29:42 -0300

    Agr vai

--- desafio2.sql
+++ desafio2.sql
@@ -1,3 +1,5 @@
-SELECT COUNT(DISTINCT musica.id_musica) AS cancoes, COUNT(DISTINCT album.id_artista) AS artistas, COUNT(DISTINCT musica.id_album) AS albuns
+SELECT COUNT(DISTINCT musica.id_musica) AS cancoes,
+COUNT(DISTINCT album.id_artista) AS artistas,
+COUNT(DISTINCT musica.id_album) AS albuns
 FROM SpotifyClone.musica AS musica
 INNER JOIN SpotifyClone.album AS album;
